8310c9308efbc0db35290150e0a906e396e3948c
Ansuman Jaiswal <ansumanjaiswal916@gmail.com>
2026-06-11 14:21:58 +0800

    Update __init__.py

--- bharatutils/__init__.py
+++ bharatutils/__init__.py
@@ -2,7 +2,7 @@
 
 __version__ = "0.1.2"
 
-from .number_format import to_lakh, to_crore, format_inr
+from .number_format import to_lakh, to_crore, format_inr, indian_commas
 from .gst import validate_gstin, validate_gstin_strict, parse_gstin
 from .pan import validate_pan, parse_pan
 from .address import parse_address, extract_pincode, pincode_to_state
